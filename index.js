d576e7d4757092a417abb62fd24ec9d116a87015
cmsosa015 <cmsosa015@gmail.com>
2021-06-17 00:53:45 +0000

    functions

---
+++ index.js
@@ -0,0 +1,12 @@
+function changenumber() {
+    let arr1 = [1,2,3,4,5,6,7,8,9]
+    let random = Math.floor(Math.random() * arr1.length);
+    document.querySelector(".number").innerHTML = arr1[random]
+}
+
+function suit() {
+    return Math.floor(Math.random() * 4 + 1);
+    switch (card){
+          
+}
+suit();
