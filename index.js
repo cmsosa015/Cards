4bcbe978d7c762a8c4fd440e13e6799be75ba9b0
cmsosa015 <cmsosa015@gmail.com>
2021-06-22 01:14:24 +0000

    functions, need color and styling

--- index.js
+++ index.js
@@ -1,12 +1,33 @@
 function changenumber() {
-    let arr1 = [1,2,3,4,5,6,7,8,9]
+    let arr1 = [1,2,3,4,5,6,7,8,9,'K','Q','J']
     let random = Math.floor(Math.random() * arr1.length);
     document.querySelector(".number").innerHTML = arr1[random]
+    suit()
 }
 
+
 function suit() {
-    return Math.floor(Math.random() * 4 + 1);
-    switch (card){
-          
+    let random= Math.floor(Math.random() * 4 + 1);
+    let suit;
+    switch(random){
+
+        case 1:
+            suit= "♦"
+            break;
+         case 2:
+            suit= "♥"
+            break;
+         case 3:
+            suit= "♠"
+            break;
+        case 4:
+            suit= "♣"
+            break;
+            
+    }
+        document.querySelector(".top").innerHTML = suit;
+        document.querySelector(".bottom").innerHTML = suit;
 }
-suit();
+
+            
+
